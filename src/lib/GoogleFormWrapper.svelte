0c012c7df4ce6572d1f8808a40ee0d6ae111955d
Hunter <hunterjisha@gmail.com>
2025-08-26 21:13:19 -0500

    this is insecure

--- src/lib/GoogleFormWrapper.svelte
+++ src/lib/GoogleFormWrapper.svelte
@@ -8,7 +8,7 @@
 
     async function handle(data)
     {
-        await fetch(`https://docs.google.com/forms/d/e/1FAIpQLSerFF_37h5-xhDUla7lOmQPnLJmnZa-UxZTuIdpPbWufk_DYA/formResponse?usp=pp_url&entry.1640907474=${data.firstname || "blank"}&entry.1618305576=${data.lastname || "blank"}&entry.1253263446=${data.email || "blank"}&entry.887760246=${data.phone || "blank"}&submit=Submit`,
+        await fetch(`https://docs.google.com/forms/d/e/1FAIpQLSe_N9aCU1aftRa8MyPx2e8gugP7TPq6oQ2bu3fznbNzyH6kqw/formResponse?usp=pp_url&entry.648455676=${data.firstname || "blank"}&entry.1869834714=${data.lastname || "blank"}&entry.430230401=${data.email || "blank"}&entry.292098261=${data.phone || "blank"}&submit=Submit`,
             {
                 method: "GET",
                 headers: {
